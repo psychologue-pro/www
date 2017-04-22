252d16520dec43f46f43e15e724683fa6c822041
olivier <olivier@catchy.io>
2017-04-22 17:33:28 +0200

    Cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -220,55 +220,6 @@ mr = (function (mr, $, window, document){
 
 }(mr, jQuery, window, document));
 
-//////////////// Cookies
-mr = (function (mr, $, window, document){
-    "use strict";
-
-    mr.cookies = {
-
-        getItem: function (sKey) {
-            if (!sKey) { return null; }
-            return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-        },
-        setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-            if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-                var sExpires = "";
-                if (vEnd) {
-                  switch (vEnd.constructor) {
-                    case Number:
-                      sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-                      break;
-                    case String:
-                      sExpires = "; expires=" + vEnd;
-                      break;
-                    case Date:
-                      sExpires = "; expires=" + vEnd.toUTCString();
-                      break;
-                }
-            }
-            document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-            return true;
-        },
-        removeItem: function (sKey, sPath, sDomain) {
-            if (!this.hasItem(sKey)) { return false; }
-            document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-            return true;
-        },
-        hasItem: function (sKey) {
-            if (!sKey) { return false; }
-            return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-        },
-        keys: function () {
-            var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-            for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-            return aKeys;
-        }
-    };
-
-    return mr;
-
-}(mr, jQuery, window, document));
-
 //////////////// Maps
 mr = (function (mr, $, window, document){
     "use strict";
@@ -560,81 +511,6 @@ mr = (function (mr, $, window, document){
     };
 
     mr.parallax = {
-        documentReady : documentReady
-    };
-
-    mr.components.documentReady.push(documentReady);
-    return mr;
-
-}(mr, jQuery, window, document));
-
-//////////////// Scroll Reveal
-mr = (function (mr, $, window, document){
-    "use strict";
-
-    var documentReady = function($){
-
-        var $body = $('body');
-        if($('body[data-reveal-selectors]').length){
-            window.sr = ScrollReveal();
-        	var selectors = $body.attr('data-reveal-selectors');
-
-        	// Gather scroll reveal options
-        	var revealTiming = 1000;
-        	if($('body[data-reveal-timing]').length){
-        		revealTiming = $body.attr('data-reveal-timing');
-        	}
-
-        	// Initialize scroll reveal
-        	window.sr.reveal(''+selectors+'', { viewFactor: 0.1, duration: ''+revealTiming+'', scale: 1, mobile: false });
-
-        }
-
-    };
-
-    mr.scrollreveal = {
-        documentReady : documentReady
-    };
-
-    mr.components.documentReady.push(documentReady);
-    return mr;
-
-}(mr, jQuery, window, document));
-
-//////////////// Smoothscroll
-mr = (function (mr, $, window, document){
-    "use strict";
-
-    var documentReady = function($){
-        // Smooth scroll to inner links
-        var innerLinks = $('a.inner-link');
-
-        if(innerLinks.length){
-            innerLinks.each(function(){
-                var link = $(this);
-                var href = link.attr('href');
-                if(href.charAt(0) !== "#"){
-                    link.removeClass('inner-link');
-                }
-            });
-
-            var offset = 0;
-            if($('body[data-smooth-scroll-offset]').length){
-                offset = $('body').attr('data-smooth-scroll-offset');
-                offset = offset*1;
-            }
-
-            smoothScroll.init({
-                selector: '.inner-link',
-                selectorHeader: null,
-                speed: 750,
-                easing: 'easeInOutCubic',
-                offset: offset
-            });
-        }
-    };
-
-    mr.smoothscroll = {
         documentReady : documentReady
     };
 
